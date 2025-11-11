7e69c1595cd743a1a834436e42de72fc069a8f7a
Ken Apacible <ken.apacible@gmail.com>
2025-11-11 08:58:57 -0800

    Added onready variable for Sprite2D node
    References:
    
    - [GDScript reference > Literals | Godot documentation](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html#literals)
    - [GDScript reference > @onready annotation | Godot documentation](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html#onready-annotation)
    - [GDScript style guide > Code order | Godot documentation](https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_styleguide.html#code-order)

--- 20251108-1200_basics/first-steps/Scripts/main.gd
+++ 20251108-1200_basics/first-steps/Scripts/main.gd
@@ -8,6 +8,8 @@ var screen_width: int = 0
 var sprite_speed_x: float = 0.0
 var sprite_speed_y: float = 0.0
 
+@onready var godot_logo = $Sprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Get the screen dimensions
@@ -26,19 +28,19 @@ func _process(delta_seconds: float) -> void:
 		return
 
 	# If the sprite reaches the right or left edge of the screen
-	if $Sprite2D.position.x > screen_width or $Sprite2D.position.x < 0:
+	if godot_logo.position.x > screen_width or godot_logo.position.x < 0:
 		# Reverse its horizontal direction
 		sprite_speed_x = -sprite_speed_x
 		# Change the sprite's color to a random color
-		$Sprite2D.modulate = Color(randf(), randf(), randf())
+		godot_logo.modulate = Color(randf(), randf(), randf())
 
 	# If the sprite reaches the top or bottom edge of the screen
-	if $Sprite2D.position.y > screen_height or $Sprite2D.position.y < 0:
+	if godot_logo.position.y > screen_height or godot_logo.position.y < 0:
 		# Reverse its vertical direction
 		sprite_speed_y = -sprite_speed_y
 		# Change the sprite's color to a random color
-		$Sprite2D.modulate = Color(randf(), randf(), randf())
+		godot_logo.modulate = Color(randf(), randf(), randf())
 
 	# Update the sprite's position based on its speed and the elapsed time
-	$Sprite2D.position.x += sprite_speed_x * delta_seconds;
-	$Sprite2D.position.y += sprite_speed_y * delta_seconds;
+	godot_logo.position.x += sprite_speed_x * delta_seconds;
+	godot_logo.position.y += sprite_speed_y * delta_seconds;
