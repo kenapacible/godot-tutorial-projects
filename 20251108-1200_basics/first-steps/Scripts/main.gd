66d7cf2acb150ee4372d69ebd06ee1a0e17a85aa
Ken Apacible <ken.apacible@gmail.com>
2025-11-11 08:32:06 -0800

    Renamed variables to snake_case to conform to GDScript conventions

--- 20251108-1200_basics/first-steps/Scripts/main.gd
+++ 20251108-1200_basics/first-steps/Scripts/main.gd
@@ -1,44 +1,44 @@
 extends Node2D
 
 # Screen dimensions
-var screenHeight: int = 0
-var screenWidth: int = 0
+var screen_height: int = 0
+var screen_width: int = 0
 
 # Speed of the sprite in pixels per second
-var spriteSpeedX: float = 0.0
-var spriteSpeedY: float = 0.0
+var sprite_speed_x: float = 0.0
+var sprite_speed_y: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Get the screen dimensions
-	screenHeight = get_viewport().size.y
-	screenWidth = get_viewport().size.x
+	screen_height = get_viewport().size.y
+	screen_width = get_viewport().size.x
 
 	# Initialize the sprite's speed based on the screen size
-	spriteSpeedX = screenWidth / 2.0  # Move half the screen width per second
-	spriteSpeedY = screenHeight / 3.0  # Move half of the screen height per second
+	sprite_speed_x = screen_width / 2.0  # Move half the screen width per second
+	sprite_speed_y = screen_height / 3.0  # Move half of the screen height per second
 
 
-# Called every frame. 'deltaSeconds' is the elapsed time since the previous frame.
-func _process(deltaSeconds: float) -> void:
+# Called every frame. 'delta_seconds' is the elapsed time since the previous frame.
+func _process(delta_seconds: float) -> void:
 	# Make sure the screen dimensions are valid
-	if screenHeight == 0 or screenWidth == 0:
+	if screen_height == 0 or screen_width == 0:
 		return
 
 	# If the sprite reaches the right or left edge of the screen
-	if $Sprite2D.position.x > screenWidth or $Sprite2D.position.x < 0:
+	if $Sprite2D.position.x > screen_width or $Sprite2D.position.x < 0:
 		# Reverse its horizontal direction
-		spriteSpeedX = -spriteSpeedX
+		sprite_speed_x = -sprite_speed_x
 		# Change the sprite's color to a random color
 		$Sprite2D.modulate = Color(randf(), randf(), randf())
 
 	# If the sprite reaches the top or bottom edge of the screen
-	if $Sprite2D.position.y > screenHeight or $Sprite2D.position.y < 0:
+	if $Sprite2D.position.y > screen_height or $Sprite2D.position.y < 0:
 		# Reverse its vertical direction
-		spriteSpeedY = -spriteSpeedY
+		sprite_speed_y = -sprite_speed_y
 		# Change the sprite's color to a random color
 		$Sprite2D.modulate = Color(randf(), randf(), randf())
 
 	# Update the sprite's position based on its speed and the elapsed time
-	$Sprite2D.position.x += spriteSpeedX * deltaSeconds;
-	$Sprite2D.position.y += spriteSpeedY * deltaSeconds;
+	$Sprite2D.position.x += sprite_speed_x * delta_seconds;
+	$Sprite2D.position.y += sprite_speed_y * delta_seconds;
