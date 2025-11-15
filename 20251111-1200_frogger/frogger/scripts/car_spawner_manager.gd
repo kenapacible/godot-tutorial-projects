8c5cb2cbae76244f1aeedf008a7844a30ec9130c
Ken Apacible <ken.apacible@gmail.com>
2025-11-14 16:21:04 -0800

    Moved car spawner logic to car manager script

--- 20251111-1200_frogger/frogger/scripts/car_spawner_manager.gd
+++ 20251111-1200_frogger/frogger/scripts/car_spawner_manager.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+const SCENE_CAR_RED: PackedScene = preload("res://scenes/car.tscn")
+
+@export var spawn_parent: Node2D
+
 @onready var car_spawners_container: Node2D = $CarSpawners
 
 # Called when the node enters the scene tree for the first time.
@@ -13,8 +17,13 @@ func _process(_delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
-	var car_spawner = car_spawners_container.get_children().pick_random() as CarSpawner
-	car_spawner.spawn()
+	if !spawn_parent:
+		return
+	var spawn_marker = car_spawners_container.get_children().pick_random() as Marker2D
+	var new_car = SCENE_CAR_RED.instantiate() as Car
+	new_car.direction = Vector2.LEFT
+	new_car.position = spawn_marker.position
+	spawn_parent.add_child(new_car)
 
 
 func _on_body_entered(body: Node2D) -> void:
