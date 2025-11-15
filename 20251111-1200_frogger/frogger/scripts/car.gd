a1923d431f04d9ed9ec1830e66e903ca3bd4caa7
Ken Apacible <ken.apacible@gmail.com>
2025-11-14 18:40:51 -0800

    Car textures are now assigned dynamically based on car variant

--- 20251111-1200_frogger/frogger/scripts/car.gd
+++ 20251111-1200_frogger/frogger/scripts/car.gd
@@ -2,12 +2,39 @@ class_name Car
 
 extends CharacterBody2D
 
+enum CarVariant {
+	UNSET = -1,
+	GREEN,
+	RED,
+	YELLOW,
+}
+
+const VARIANT_TEXTURES: Dictionary = {
+	CarVariant.GREEN: "res://graphics/cars/green.png",
+	CarVariant.RED: "res://graphics/cars/red.png",
+	CarVariant.YELLOW: "res://graphics/cars/yellow.png",
+}
+
 var direction: Vector2 = Vector2.ZERO
 var speed_current: float = 100.0
+var variant: CarVariant:
+	get:
+		return variant
+	set(value):
+		variant = value
+		if sprite_renderer and value != CarVariant.UNSET:
+			sprite_renderer.texture = load(
+				VARIANT_TEXTURES.get(value, "res://graphics/cars/red.png")
+			)
+
+@onready var sprite_renderer: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if variant != CarVariant.UNSET:
+		sprite_renderer.texture = load(
+			VARIANT_TEXTURES.get(variant, "res://graphics/cars/red.png")
+		)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- 20251111-1200_frogger/frogger/scripts/car_manager.gd
+++ 20251111-1200_frogger/frogger/scripts/car_manager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SCENE_CAR_RED: PackedScene = preload("res://scenes/car.tscn")
+const CAR_SCENE: PackedScene = preload("res://scenes/car.tscn")
 
 @export var spawn_parent: Node2D
 
@@ -20,9 +20,14 @@ func _on_timer_timeout() -> void:
 	if !spawn_parent:
 		return
 	var spawn_marker = car_spawners_container.get_children().pick_random() as Marker2D
-	var new_car = SCENE_CAR_RED.instantiate() as Car
+	var new_car = CAR_SCENE.instantiate() as Car
 	new_car.direction = Vector2.LEFT
 	new_car.position = spawn_marker.position
+	new_car.variant = [
+		Car.CarVariant.GREEN,
+		Car.CarVariant.RED,
+		Car.CarVariant.YELLOW,
+	].pick_random()
 	spawn_parent.add_child(new_car)
 
 
